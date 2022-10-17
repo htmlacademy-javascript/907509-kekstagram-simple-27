7b4e18cf2b215b52ebd4be67887d8aae77afd18b
Amanter <mister_x82@mail.ru>
2022-10-17 09:04:20 +0300

    Сгенерировал моки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+const ARRAY_NUMBER = 25;
+
+const DESCRIPTIONS = [
+  'Фотография природы',
+  'красиво озеро',
+  'Универмаг',
+  'Детская площадка',
+  'Лодочная станция',
+  'ресторан',
+  'Торговый зал',
+];
+
 const getRandomInt = function (min, max) {
   if (min < 0 || min >= max) {
-    return NaN;
+    return 'Введено число меньше 0 либо задан неправильный интервал';
   }
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -9,9 +21,17 @@ const getRandomInt = function (min, max) {
 
 getRandomInt(0, 100);
 
-const getStringLenth = function (string, maxLength) {
-  const stringLenth = string.split('');
-  return stringLenth.length < maxLength;
-};
-
+const getStringLenth = (string, maxLength) => string.length <= maxLength;
 getStringLenth('find string length', 5);
+
+const getPhotoDescription = (num) => ({
+  id: num + 1,
+  url: `'photos/${num + 1}.jpg'`,
+  description: getRandomInt(0, DESCRIPTIONS.length - 1),
+  likes: getRandomInt(15, 200),
+  comments: getRandomInt(0, 200),
+});
+
+const photoDescriptions = Array.from({length: ARRAY_NUMBER}, (value, num) => getPhotoDescription(num++));
+
+export {photoDescriptions};
