3f86cbd00051f7488fa7a2f3744f9b48065e2165
Amanter <mister_x82@mail.ru>
2022-11-25 12:57:24 +0300

    Доработка ошибок

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,25 +1,33 @@
 import {getStringLength} from './utils.js';
+import {sendData, onFormSuccessSend, onFormErrorSend} from './server-interaction.js';
 
-const MIN_COMMENTS_LENGHT = 20;
-const MAX_COMMENTS_LENGHT = 140;
+
+const MIN_LENGHT = 20;
+const MAX_LENGHT = 140;
 const INVALID_INPUT_STYLE = 'red';
 const VALID_INPUT_STYLE = 'green';
 
+const imgUploadForm = document.querySelector('.img-upload__form');
 const inputComment = document.querySelector('.text__description');
 
-const onValidationComment = (evt) => {
-  const isValidationTextLength = getStringLength(inputComment.value, MIN_COMMENTS_LENGHT, MAX_COMMENTS_LENGHT);
+const onValidationComment = () => {
+  imgUploadForm.addEventListener('input', () => {
+    const isValidationTextLength = getStringLength(inputComment.value, MIN_LENGHT, MAX_LENGHT);
 
-  if(!isValidationTextLength) {
-    inputComment.setCustomValidity(`Комментарий обязателен и не может быть меньше ${MIN_COMMENTS_LENGHT} символов,
-     и не может быть больше ${MAX_COMMENTS_LENGHT} символов.`);
-    inputComment.style.outlineColor = INVALID_INPUT_STYLE;
-    evt.preventDefault();
-  } else {
-    inputComment.setCustomValidity('');
-    inputComment.style.outlineColor = VALID_INPUT_STYLE;
-  }
-  inputComment.reportValidity();
+    if(!isValidationTextLength) {
+      inputComment.setCustomValidity(`Комментарий обязателен и не может быть меньше ${MIN_LENGHT} символов,
+     и не может быть больше ${MAX_LENGHT} символов.`);
+      inputComment.style.outlineColor = INVALID_INPUT_STYLE;
+    } else {
+      inputComment.setCustomValidity('');
+      inputComment.style.outlineColor = VALID_INPUT_STYLE;
+    }
+
+    inputComment.reportValidity();
+  });
 };
 
-inputComment.addEventListener('input', onValidationComment);
+onValidationComment();
+
+sendData(onFormSuccessSend, onFormErrorSend);
+
--- js/render-pics.js
+++ js/render-pics.js
@@ -2,13 +2,13 @@ const template = document.querySelector('#picture').content.querySelector('.pict
 const pictureContainer = document.querySelector('.pictures');
 const fragment = document.createDocumentFragment();
 
-const renderPictures = (array) => array.forEach(({url, likes, comments}) => {
+const renderPictures = (array) => { array.forEach(({url, likes, comments}) => {
   const templateItem = template.cloneNode(true);
   templateItem.querySelector('.picture__img').src = url;
   templateItem.querySelector('.picture__likes').textContent = likes;
   templateItem.querySelector('.picture__comments').textContent = comments;
   fragment.append(templateItem);
-  pictureContainer.append(fragment);
 });
+pictureContainer.append(fragment);};
 
 export{renderPictures};
